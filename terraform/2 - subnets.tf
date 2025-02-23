2692e996375a89c7623e41760f05fd02ab6d0fa1
Léo Saglia <leosaglia@hotmail.com>
2025-02-23 18:35:45 -0300

    ajustar nome local

--- terraform/2 - subnets.tf
+++ terraform/2 - subnets.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_blocks)
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_blocks[count.index]
-  availability_zone       = locals.availability_zones[count.index]
+  availability_zone       = local.availability_zones[count.index]
   map_public_ip_on_launch = true
 
   tags = {
@@ -15,7 +15,7 @@ resource "aws_subnet" "private" {
   count                   = length(var.private_subnet_blocks)
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.private_subnet_blocks[count.index]
-  availability_zone       = locals.availability_zones[count.index]
+  availability_zone       = local.availability_zones[count.index]
 
   tags = {
     Name = "tech-challenge-PrivateSubnet${count.index + 1}"
