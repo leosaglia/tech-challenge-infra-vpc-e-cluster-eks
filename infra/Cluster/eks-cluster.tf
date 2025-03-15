2d4e76a8d034689434fb8fc8730b78b3eced8fc0
Léo Saglia <leosaglia@hotmail.com>
2025-03-15 16:35:48 -0300

    adicionar permissao para comunicacao com RDS

--- infra/Cluster/eks-cluster.tf
+++ infra/Cluster/eks-cluster.tf
@@ -9,3 +9,16 @@ resource "aws_eks_cluster" "eks" {
         subnet_ids              = var.private_subnets
     }
 }
+
+data "aws_db_instance" "rds_instance" {
+  db_instance_identifier = "tech-challenge-fast-food-postgres"
+}
+
+resource "aws_security_group_rule" "allow_app_to_rds" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  security_group_id        = tolist(data.aws_db_instance.rds_instance.vpc_security_groups)[0]
+  source_security_group_id = aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
+}
