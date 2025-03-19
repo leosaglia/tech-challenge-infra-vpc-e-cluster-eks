2753dec60bce48be44b899a8fae70eef20b29e79
Léo Saglia <leosaglia@hotmail.com>
2025-03-19 20:57:49 -0300

    atualizar nome bucket

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "state-terraform-tech"
+    bucket = "state-terraform-tech-v2"
     key = "tech-challenge-infra-vpc-e-cluster-eks/terraform.tfstate"
     region = "us-east-1"
     encrypt = true
