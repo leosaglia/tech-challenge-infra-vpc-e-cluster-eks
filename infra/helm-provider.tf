c4c5f9553225a2a927d0d887e8b58445732faf5f
Leonardo Saglia do Nascimento Silva <leosaglia@hotmail.com>
2025-04-02 14:12:35 -0300

    remove config on helm-provider.tf

--- infra/helm-provider.tf
+++ infra/helm-provider.tf
@@ -13,6 +13,5 @@ provider "helm" {
     host                   = data.aws_eks_cluster.eks.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.eks.token
-    config_path            = "~/.kube/config"
   }
 }
